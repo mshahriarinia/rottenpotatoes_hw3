64c7dbb740280f0191c5cd76bb35c01b9b184529
mshahriarinia <info@mshahriarinia.com>
2014-03-14 23:27:51 -0700

    debug

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+config.assets.initialize_on_precompile = false
+
 module Rottenpotatoes
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
