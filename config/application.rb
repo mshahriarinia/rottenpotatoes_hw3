b20aaff0e014151056806850b8ba4f7c9b45df8a
mshahriarinia <info@mshahriarinia.com>
2014-03-14 23:36:25 -0700

    debug

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-config.assets.initialize_on_precompile = false
+#config.assets.initialize_on_precompile = false
 
 module Rottenpotatoes
   class Application < Rails::Application
